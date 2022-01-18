8c93c8b736487b3164a1b7eed927cb0fa926ce3b
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-01-18 15:52:56 -0700

    calculator fully functional

--- calculator/calc.js
+++ calculator/calc.js
@@ -14,7 +14,31 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	// console.log("Math Operation: ", mathSymbol);
+	// console.log("First Number: ", num1);
+	// console.log("Second Number: ", num2);
+	
 
+	if (mathSymbol === '+')
+	{
+		console.log("Addition of numbers is: ", num1 + num2);
+	}
+	else if(mathSymbol === '-')
+	{
+		console.log("Subtraction of numbers is: ", num1 - num2);
+	}
+	else if (mathSymbol === '*')
+	{
+		console.log("Multiplication of numbers is: ", num1 * num2);
+	}
+	else if (mathSymbol === "/")
+	{
+		console.log("Division of numbers is: ", num1 / num2);
+	}
+	else if (mathSymbol === "sqrt")
+	{
+		console.log("Square root of number: ", Math.sqrt(num1));
+	}
 	// This line closes the connection to the command line interface.
 	reader.close()
 
